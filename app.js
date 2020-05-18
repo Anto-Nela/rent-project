6d451e953103639a5982c61f518cb69d6c49551b
Anto-Nela <batmanmyman1@gmail.com>
2020-05-18 14:53:19 +0200

    tried sth else

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ require('dotenv/config');
    app.use(express.urlencoded({ extended: true }));
    app.use(express.json());
    
+   app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+    
+  });
     app.use(allservice);
    
   
