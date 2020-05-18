b8f4e0c0ec91bcc74dadf2f4fbea1ab314885a4e
Anto-Nela <batmanmyman1@gmail.com>
2020-05-18 15:44:40 +0200

    fixed 0 rooms and people

--- functions/get/searchHomes.js
+++ functions/get/searchHomes.js
@@ -10,7 +10,7 @@ function searchHomes(db,maxValue,minValue,rruga,nrdhoma,nrpersona,cb){
    persona = parseInt(nrpersona, 10);
    
   try{
-    
+
 //rasti kur useri ve personat (roommates) dhe dhomat 0
   if(persona==0&&dhoma==0){
 
@@ -82,6 +82,31 @@ function searchHomes(db,maxValue,minValue,rruga,nrdhoma,nrpersona,cb){
         });
       }
 
+    //kur useri ve dhoma dhe persona(roommates) me shum se 4 
+     else if(dhoma==4 && persona==4){
+
+      db.collection("homes").find(
+        { 'adress.rruga': {'$regex' : '.*' + rruga + '.*'},
+           cmimi: { $gte: minprice, $lte: maxprice },
+           'nr_personash': {$gte:3},
+           'nr_dhomash': {$gte:3}
+      }).toArray((err,doc)=>{
+        if(err){
+          myError.Error(db,'home',200,4,(error,pls)=>{
+            if(error) cb(error);
+            else cb(pls);
+          });  
+        } 
+        if (!doc.length){
+          myError.Error(db,'home',200,4,(error,pls)=>{
+            if(error) cb(error);
+            else cb(pls);
+          });  
+        }
+        else cb(null, doc);
+      });
+    }
+
       //kur useri ve dhomat me shum se 4
      else if(dhoma==4){
 
@@ -130,32 +155,6 @@ function searchHomes(db,maxValue,minValue,rruga,nrdhoma,nrpersona,cb){
           else cb(null, doc);
         });
       }
-
-      //kur useri ve dhoma dhe persona(roommates) me shum se 4 
-     else if(dhoma==4 && persona==4){
-
-        db.collection("homes").find(
-          { 'adress.rruga': {'$regex' : '.*' + rruga + '.*'},
-             cmimi: { $gte: minprice, $lte: maxprice },
-             'nr_personash': {$gte:3},
-             'nr_dhomash': {$gte:3}
-        }).toArray((err,doc)=>{
-          if(err){
-            myError.Error(db,'home',200,4,(error,pls)=>{
-              if(error) cb(error);
-              else cb(pls);
-            });  
-          } 
-          if (!doc.length){
-            myError.Error(db,'home',200,4,(error,pls)=>{
-              if(error) cb(error);
-              else cb(pls);
-            });  
-          }
-          else cb(null, doc);
-        });
-      }
-      
 
     else{
      
